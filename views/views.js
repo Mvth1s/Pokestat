750cd57d65cfffcf7a6928e248aea07174d5ea92
Mvtos <aguadomathis@gmail.com>
2024-11-23 17:55:42 +0100

    correction css

--- views/views.js
+++ views/views.js
@@ -1,23 +1,27 @@
 // views/views.js
 class PokemonView {
-    constructor() {
-        this.pokemonContainer = document.getElementById("cards-container");
-    }
+	constructor() {
+		this.pokemonContainer = document.getElementById("cards-container");
+	}
 
-    renderPokemons(pokemons) {
-        this.pokemonContainer.innerHTML = ''; // Clear previous content
-        const seenPokemon = new Set();
-        pokemons.forEach(result => {
-            if (!seenPokemon.has(result.pokemon)) {
-                seenPokemon.add(result.pokemon);
-                const pokemonCard = document.createElement("div");
-                pokemonCard.className = "pokemon-card";
-                pokemonCard.innerHTML = `
-                    <h1><a href="${result.pokemon}">${result.name}</a></h1>
+	renderPokemons(pokemons) {
+		this.pokemonContainer.innerHTML = ""; // Clear previous content
+		const seenPokemon = new Set();
+		pokemons.forEach((result) => {
+			if (!seenPokemon.has(result.pokemon)) {
+				seenPokemon.add(result.pokemon);
+				const pokemonCard = document.createElement("div");
+				pokemonCard.className = "pokemon-card";
+				pokemonCard.innerHTML = `
+                    <h1><a href="${result.pokemon}" class="PokemonName">${
+					result.name
+				}</a></h1>
                     <p>N° Pokédex : ${result.number}</p>
                     <img src="${result.image}" alt="${result.name}">
                     <div class="pokemon-types">
-                        <strong>Types:</strong> ${result.typeName1 ? result.typeName1 : ''}${result.typeName2 ? ', ' + result.typeName2 : ''}
+                        ${result.typeName1 ? result.typeName1 : ""}${
+					result.typeName2 ? ", " + result.typeName2 : ""
+				}
                     </div>
                     <h2>Attributs:</h2>
                     <ul class="stats">
@@ -27,8 +31,8 @@ class PokemonView {
                         <li>Vitesse: ${result.speed}</li>
                     </ul>
                 `;
-                this.pokemonContainer.appendChild(pokemonCard);
-            }
-        });
-    }
+				this.pokemonContainer.appendChild(pokemonCard);
+			}
+		});
+	}
 }
