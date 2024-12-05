a388b7a7b0262f69d37ffe83348148351f033ec1
ahemer1 <alexy-alan.hemery@universite-paris-saclay.fr>
2024-12-05 16:57:21 +0100

    Le selector des générations fonctionnent let's gooooooo

--- controller/controller.js
+++ controller/controller.js
@@ -9,7 +9,8 @@ class PokemonController {
     async init() {
         await this.model.fetchPokemons();
         this.updateView();
-        this.setupEventListeners();
+        this.setupEventListenersByType();
+        this.setupEventListenersByGen();
     }
 
     updateView() {
@@ -17,11 +18,23 @@ class PokemonController {
         this.view.renderPokemons(pokemons);
     }
 
-    setupEventListeners() {
+    setupEventListenersByType() {
         const typeSelector = document.getElementById("type-selector");
+        const genSelector = document.getElementById("gen-selector");
         typeSelector.addEventListener("change", async (event) => {
             const selectedType = event.target.value;
+            genSelector.value = "all";
             await this.filterPokemonsByType(selectedType);
+        });
+    }
+
+    setupEventListenersByGen() {
+        const genSelector = document.getElementById("gen-selector");
+        const typeSelector = document.getElementById("type-selector");
+        genSelector.addEventListener("change", async (event) => {
+            const selectedGen = event.target.value;
+            typeSelector.value = "all";
+            await this.filterPokemonsByGen(selectedGen);
         });
     }
 
@@ -30,6 +43,44 @@ class PokemonController {
             this.updateView();
         } else {
             await this.statModel.fetchPokemonsByType(type);
+            const filteredPokemons = this.statModel.getPokemons();
+            this.view.renderPokemons(filteredPokemons);
+        }
+    }
+
+    async filterPokemonsByGen(gen) {
+        if (gen === "all") {
+            this.updateView();
+        } else {
+            let genMin = 0, genMax = 0;
+            if(gen === "1") {genMin = 1; genMax = 151;}
+            else if(gen === "2") {genMin = 152; genMax = 251;}
+            else if(gen === "3") {genMin = 252; genMax = 386;}
+            else if(gen === "4") {genMin = 387; genMax = 493;}
+            
+            /*
+            switch (gen) {
+                case "1":
+                    genMin = 1; 
+                    genMax = 151;
+                    break;
+                case "2":
+                    genMin = 152; 
+                    genMax = 251;
+                    break;
+                case "3":
+                    genMin = 252; 
+                    genMax = 386;
+                    break;
+                case "4":
+                    genMin = 387; 
+                    genMax = 493;
+                    break;
+                default:
+                    break;
+            }*/
+
+            await this.statModel.fetchPokemonsByGen(genMin, genMax);
             const filteredPokemons = this.statModel.getPokemons();
             this.view.renderPokemons(filteredPokemons);
         }
